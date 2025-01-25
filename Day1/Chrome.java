7045ab80919a65919bcea27ecb8be76d47ac0839
Kavipriyasakthivel <100136005+Kavipriyasakthivel@users.noreply.github.com>
2025-01-25 13:12:08 +0530

    Add files via upload
    Day1

---
+++ Day1/Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//float datatype
+		float version = 91.0f;
+		//String datatype
+		String developer = "Google";
+		//Boolean datatype
+		Boolean isBeta = false;
+		//int datatype
+		int releaseYear = 2008;
+		//char datatype
+		char shortcutKey = 'C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+
+	}
+
+}
---
+++ Day1/LaunchBrowser.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		driver.close();
+	}
+
+}
